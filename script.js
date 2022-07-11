5e4cff6d5125a2cec178b37bba16ce39e937ea94
matheusfelipetp <matheusfelipetp@outlook.com>
2022-07-11 18:50:36 -0300

    Projeto inicial

---
+++ script.js
@@ -0,0 +1,133 @@
+let tasks = [];
+
+const lista = document.querySelector(".task__list");
+const btnCriarTask = document.querySelector("#btnCriarTask");
+const inputTask = document.querySelector(".form-options input");
+const inputOption = document.querySelector("#task-option");
+const paragrafo = document.querySelector(".task-itens p");
+const inputSearch = document.getElementById("search");
+const btnSearch = document.getElementById("btnSearch");
+
+function adicionarElemento(event) {
+  event.preventDefault();
+
+  paragrafo.classList.add("desativo");
+
+  let prioridadeItem;
+  if (inputOption.value === "Urgente") {
+    prioridadeItem = 1;
+  }
+  if (inputOption.value === "Prioritário") {
+    prioridadeItem = 2;
+  }
+  if (inputOption.value === "Normal" || inputOption.value === "valor1") {
+    prioridadeItem = 3;
+  }
+
+  const tarefa = {
+    titulo: inputTask.value,
+    tipo: inputOption.value,
+    prioridade: prioridadeItem,
+  };
+
+  tasks.push(tarefa);
+  organizarLista(tasks);
+  listarElemento(tasks);
+}
+
+function criarElemento({ titulo, tipo }) {
+  const itemLista = document.createElement("li");
+  const span = document.createElement("span");
+  const div = document.createElement("div");
+  const titleItem = document.createElement("h2");
+  const button = document.createElement("button");
+  const buttonImg = document.createElement("img");
+
+  titleItem.innerText = titulo;
+  if (tipo.toLowerCase() === "urgente") {
+    span.style = "background: var(--urgente)";
+  } else if (tipo.toLowerCase() === "prioritário") {
+    span.style = "background: #77e864;";
+  } else {
+    span.style = "background: #d6e340;";
+  }
+
+  buttonImg.src = "./img/delete_FILL0_wght400_GRAD0_opsz48.svg";
+
+  button.appendChild(buttonImg);
+  div.appendChild(span);
+  div.appendChild(titleItem);
+  itemLista.appendChild(div);
+  itemLista.appendChild(button);
+
+  return itemLista;
+}
+
+function organizarLista(list) {
+  list.sort((a, b) => {
+    if (a.prioridade > b.prioridade) {
+      return 1;
+    }
+    if (a.prioridade < b.prioridade) {
+      return -1;
+    }
+  });
+}
+
+function listarElemento(list) {
+  lista.innerHTML = "";
+
+  list.forEach((item) => {
+    const template = criarElemento(item);
+    lista.appendChild(template);
+  });
+}
+
+function habilitarBotao() {
+  let content = inputTask.value;
+
+  if (content !== null && content.trim() !== "") {
+    btnCriarTask.disabled = false;
+  } else {
+    btnCriarTask.disabled = true;
+  }
+}
+
+function removerElemento(event) {
+  const btnRemove = event.target;
+
+  if (btnRemove.tagName === "BUTTON" || btnRemove.tagName === "IMG") {
+    btnRemove.closest("li").remove();
+    tasks.splice("li", 1);
+  }
+
+  if (lista.innerHTML == "") {
+    paragrafo.classList.remove("desativo");
+  }
+}
+
+function filtrarElementos(event) {
+  event.preventDefault();
+
+  const pesquisaUser = inputSearch.value;
+  const resultadoBusca = buscarElemento(pesquisaUser);
+
+  listarElemento(resultadoBusca);
+}
+
+function buscarElemento(valorPesquisa) {
+  let arrayBusca = [];
+
+  tasks.forEach((item) => {
+    if (valorPesquisa.toLowerCase() == item.titulo.toLowerCase()) {
+      arrayBusca.push(item);
+    }
+  });
+
+  return arrayBusca;
+}
+
+btnCriarTask.addEventListener("click", adicionarElemento);
+inputTask.addEventListener("input", habilitarBotao);
+lista.addEventListener("click", removerElemento);
+btnSearch.addEventListener("click", filtrarElementos);
